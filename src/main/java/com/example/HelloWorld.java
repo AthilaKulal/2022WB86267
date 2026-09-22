aaa9894d5059ba831420f801777370577fd16a3e
AthilaKulal <athilac4@gmail.com>
2026-09-22 20:49:55 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,6 +3,7 @@ package com.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World from Jenkins CI/CD!");
+        System.out.println("Configured jenkins build polling");
         System.out.println("Build successful!");
     }
 
