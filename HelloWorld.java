e73d8e395b2387581aebb49b614feee2f2cdcf47
AthilaKulal <athilac4@gmail.com>
2026-09-22 10:20:29 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Jenkins CI/CD!");
+        System.out.println("Build successful!");
+    }
+
+    public static String getGreeting() {
+        return "Hello World";
+    }
+}
